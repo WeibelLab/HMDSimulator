501823af163c943c8aec47b80c0370d41c4dda17
WeichenLiu <WeichenLiu@users.noreply.github.com>
2020-02-10 09:32:00 -0800

    Initial commit

---
+++ Assets/Scripts/TransformUpdater.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformUpdater : MonoBehaviour
+{
+
+    public GameObject originalGo;
+
+    private TransformHandler th;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        th = originalGo.GetComponent<TransformHandler>();
+    }
+    
+    void LateUpdate()
+    {
+        if (th)
+        {
+            transform.localPosition = th.GetLocalTranslation();
+            transform.localRotation = th.GetLocalRotation();
+            transform.localScale = th.GetLocalScale();
+        }
+        else
+        {
+            transform.localPosition = originalGo.transform.localPosition;
+            transform.localRotation = originalGo.transform.localRotation;
+            transform.localScale = originalGo.transform.localScale;
+        }
+    }
+}
---
+++ Assets/Scripts/VirtualSceneInitializer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualSceneInitializer : MonoBehaviour
+{
+
+    public GameObject RealWorld;
+    public Camera LeftEye;
+    public Camera RightEye;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (RealWorld != null)
+        {
+            TraverseHierarchy(RealWorld.transform, transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TraverseHierarchy(Transform parent, Transform mirrorParent) {
+        foreach (Transform child in parent.transform)
+        {
+            // Debug.Log(child.name);
+
+            // Copy current Node
+            GameObject go = Instantiate(child.gameObject);
+
+            // Remove unneeded components
+            foreach (var c in go.GetComponents(typeof(Component)))
+            {
+                if (!(c is Transform) && !(c is MeshFilter) && !(c is MeshRenderer))
+                {
+                    Destroy(c);
+                }
+            }
+
+            // Attach updater script
+            TransformUpdater tu = go.AddComponent<TransformUpdater>();
+
+            // Set updater field
+            tu.originalGo = child.gameObject;
+
+            // Set hierarchy
+            go.transform.parent = mirrorParent;
+
+            // Remove duplicated children
+            foreach (Transform goChild in go.transform)
+            {
+                Destroy(goChild.gameObject);
+            }
+
+            // Attach camera if it's display
+            if (child.name == "LeftEye")
+            {
+                var updater = LeftEye.gameObject.AddComponent<TransformUpdater>();
+                updater.originalGo = go;
+            }
+            if (child.name == "RightEye")
+            {
+                var updater = RightEye.gameObject.AddComponent<TransformUpdater>();
+                updater.originalGo = go;
+            }
+
+            TraverseHierarchy(child, go.transform);
+        }
+    }
+}
