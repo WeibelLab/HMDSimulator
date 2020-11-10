a96bf16a46e0520e09840ba71e7abaad6e74acd0
Danilo Gasques <danilod100@gmail.com>
2020-11-09 16:04:11 -0800

    Augmenting ExperimentResult with more information - data points collected, time taken, etc...

--- Assets/Scripts/ExperimentResult.cs
+++ Assets/Scripts/ExperimentResult.cs
@@ -4,16 +4,38 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+/// <summary>
+/// ExperimentResult is a helper class used to serialize the projection matrix obtained
+/// after a calibration experiment.
+/// </summary>
 [Serializable]
 public class ExperimentResult
 {
-    //[SerializeField] public float completionTime;
+    [SerializeField] public float completionTime = 0.0f;
+    [SerializeField] public DateTime startTime = new DateTime();
+    [SerializeField] public DateTime endTime;
+     
     [SerializeField] public Matrix4x4 projectionMatrixLeft;
     [SerializeField] public Matrix4x4 projectionMatrixRight;
     [SerializeField] public Matrix4x4 groundTruthProjectionMatrixLeft;
     [SerializeField] public Matrix4x4 groundTruthProjectionMatrixRight;
+
+
     [SerializeField] public Vector3 errorLeft;
     [SerializeField] public Vector3 errorRight;
+    [SerializeField] public int pointsCollected = 0;
+
+
+    /// <summary>
+    /// A number identifying the type of calibration performed
+    /// </summary>
+    [SerializeField] public int calibrationModality = -1;
+
+    
+    /// <summary>
+    /// A string identifying the type of calibration performed
+    /// </summary>
+    [SerializeField] public string calibrationModalityStr = "undefined";
 
     public static ExperimentResult CreateFromJSON(string jsonString)
     {
@@ -28,6 +50,7 @@ public class ExperimentResult
     public static void SaveToDrive(ExperimentResult input, string path)
     {
         string jsonString = ConvertToJSON(input);
-        File.WriteAllText(path, jsonString);
+        Debug.Log(jsonString);
+        //File.WriteAllText(path, jsonString);
     }
 }
